ed9837297d1be81267086a8377b8608463dd454e
tsung <tsunghao@gmail.com>
2022-09-14 11:10:08 +0800

    forget remove debug for comment command

--- shell/init.sh
+++ shell/init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 dirname=$(dirname "$0")
 cd $dirname
-#cp -iv linux/bashrc ~/.bashrc
-#cp -iv linux/profile ~/.profile
-#cp -iv gitconfig ~/.gitconfig
-#cp -iv gitignore ~/.gitignore
-#cp -iv screenrc ~/.screenrc
-#cp -iv tmux.conf ~/.tmux.conf
-#cp -iv vimrc ~/.vimrc
+cp -iv linux/bashrc ~/.bashrc
+cp -iv linux/profile ~/.profile
+cp -iv gitconfig ~/.gitconfig
+cp -iv gitignore ~/.gitignore
+cp -iv screenrc ~/.screenrc
+cp -iv tmux.conf ~/.tmux.conf
+cp -iv vimrc ~/.vimrc
 if [ ! -d `realpath ~/.vim` ]; then
     cp -r -iv vim ~/.vim
 fi
