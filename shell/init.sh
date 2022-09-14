80cce9273cfb5e606856860322c741f415980992
tsung <tsunghao@gmail.com>
2022-09-14 11:07:40 +0800

    add realpath check directory exists

--- shell/init.sh
+++ shell/init.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 dirname=$(dirname "$0")
 cd $dirname
-cp -iv linux/bashrc ~/.bashrc
-cp -iv linux/profile ~/.profile
-cp -iv gitconfig ~/.gitconfig
-cp -iv gitignore ~/.gitignore
-cp -iv screenrc ~/.screenrc
-cp -iv tmux.conf ~/.tmux.conf
-cp -iv vimrc ~/.vimrc
-if [ ! -d "~/.vim" ]; then
+#cp -iv linux/bashrc ~/.bashrc
+#cp -iv linux/profile ~/.profile
+#cp -iv gitconfig ~/.gitconfig
+#cp -iv gitignore ~/.gitignore
+#cp -iv screenrc ~/.screenrc
+#cp -iv tmux.conf ~/.tmux.conf
+#cp -iv vimrc ~/.vimrc
+if [ ! -d `realpath ~/.vim` ]; then
     cp -r -iv vim ~/.vim
 fi
-if [ ! -d "~/vi_backup" ]; then
+if [ ! -d `realpath ~/vi_backup` ]; then
     mkdir ~/vi_backup # for .vimrc backupdir=~/vi_backup/
 fi
