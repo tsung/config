d485b0b79787077d3b41d8d3ffd4858045586437
tsung <tsunghao@gmail.com>
2022-09-14 10:15:39 +0800

    init.sh initialization env (copy to ~/.*)

---
+++ shell/init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cp -v linux/bashrc ~/.bashrc
+cp -v linux/profile ~/.profile
+cp -v gitconfig ~/.gitconfig
+cp -v gitignore ~/.gitignore
+cp -v screenrc ~/.screenrc
+cp -v tmux.conf ~/.tmux.conf
+cp -v vim ~/.vim
+cp -v vimrc ~/.vimrc
