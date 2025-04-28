a5772cd1845080e38e146ead1e99c999d80af690
tsung <tsunghao@gmail.com>
2025-04-28 10:26:31 +0800

    wezterm 滑鼠點擊不要自動開瀏覽器

--- shell/wezterm.lua
+++ shell/wezterm.lua
@@ -12,6 +12,30 @@ config.hide_mouse_cursor_when_typing = false
 -- config.initial_cols = 132
 -- config.initial_rows = 43
 -- config.line_height = 1.0
+
+-- 滑鼠點擊 URL 不要開瀏覽器
+config.mouse_bindings = {
+    -- Disable the default click behavior
+    {
+      event = { Up = { streak = 1, button = "Left"} },
+      mods = "NONE",
+      action = wezterm.action.DisableDefaultAssignment, -- You may want a different option here. See /u/Brian's comment below
+    },
+    -- Ctrl-click will open the link under the mouse cursor
+    {
+        event = { Up = { streak = 1, button = "Left" } },
+        mods = "CTRL",
+        action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+    -- Disable the Ctrl-click down event to stop programs from seeing it when a URL is clicked
+    {
+        event = { Down = { streak = 1, button = "Left" } },
+        mods = "CTRL",
+        action = wezterm.action.Nop,
+    }
+}
+-- action = wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'
+
 config.window_padding = {
     bottom = 0,
     left = 0,
